df6fd6d51ccceb148d401e4ae6b192369ee5b94e
EstacioEA <edwaraestacio06@gmail.com>
2025-10-29 17:36:34 -0500

    Segunda versión, con mucho gusto

--- Prueba Node/src/pages/Game.js
+++ Prueba Node/src/pages/Game.js
@@ -1,9 +1,10 @@
 import createBoard from '../components/board.js'
+import axios from 'axios';
 
-async function getUserData() {
-    const response = await fetch('http://localhost:3000/user')
-    console.log(response.data)
-    return response;
+async function getUserDataAxios() {
+    const registros = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
+    console.log(registros.data);
+    return registros;
 }
 
 export function Game() {
@@ -22,8 +23,19 @@ export function Game() {
     container.appendChild(board)
     container.appendChild(footer)
 
-    getUserData().then(data => {
-        document.getElementById('footer').textContent = `User data fetched: ${data.statusText}`
+    // getUserData().then(response => {
+    //     console.log('User data fetched:', response.registros)
+    //     const footer = document.getElementById('footer')
+    //     if(footer){
+    //         footer.textContent = `User Data: ${response.registros.length} records found.`
+    //     }
+    // })
+
+    getUserDataAxios().then(response => {
+        const footer = document.getElementById('footer')
+        if(footer){
+            footer.textContent = `User Data: ${response.data.cries.legacy}.`
+        }
     })
 
     return container
